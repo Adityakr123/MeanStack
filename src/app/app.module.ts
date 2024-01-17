096c2bcdb4ee22316f28a50b43a67bfdb0901fe3
Aditya <aditya.kumar@cognecto.com>
2024-01-17 14:38:33 +0530

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
-import{MatButtonModule} from '@angular/material/button'
+import{MatButtonModule} from '@angular/material/button';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{ MatExpansionModule} from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -10,7 +12,8 @@ import { AdityaComponent } from './aditya/aditya.component';
 import { C1Component } from './c1/c1.component';
 import { PostCreateComponent } from './post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';//this line 
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';//this line 
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { HeaderComponent } from './header/header.component';//this line
     AdityaComponent,
     C1Component,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +31,8 @@ import { HeaderComponent } from './header/header.component';//this line
     MatCardModule,
     AppRoutingModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
   providers: [],
